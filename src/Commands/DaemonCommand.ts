506b8526f72f1fdc1a58c9a1384d2070a65f0275
WBRK-dev <koertjrweberp@gmail.com>
2025-05-30 14:26:46 +0200

    fix: added cronjob instead of direct call

--- src/Commands/DaemonCommand.ts
+++ src/Commands/DaemonCommand.ts
@@ -3,7 +3,6 @@ import Print from "../Kernel/Support/Facades/Print.js";
 import { CronJob } from 'cron';
 import HandleCryptoTick from "../CronJobs/HandleCryptoTick.js";
 import CheckIfDaemonCanRun from "../Services/CheckIfDaemonCanRun.js";
-import StatsMail from "../Mail/StatsMail.js";
 import WeeklyReport from "../CronJobs/WeeklyReport.js";
 
 export default class LoginCommand implements Command {
@@ -13,15 +12,20 @@ export default class LoginCommand implements Command {
     async handle(args: Record<string, any>) {
         await CheckIfDaemonCanRun();
 
-        // new CronJob(
-        //     '0 0 * * * *',
-        //     HandleCryptoTick,
-        //     null,
-        //     true,
-        //     'Europe/Amsterdam',
-        // );
-        await HandleCryptoTick();
-        await WeeklyReport();
+        new CronJob(
+            '0 0 * * * *',
+            HandleCryptoTick,
+            null,
+            true,
+            'Europe/Amsterdam',
+        );
+        new CronJob(
+            '0 0 * * 0',
+            WeeklyReport,
+            null,
+            true,
+            'Europe/Amsterdam',
+        );
 
         Print.info("Daemon started");
     }
