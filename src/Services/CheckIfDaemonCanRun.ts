ff30b632fe429ed3989c6c469c6c9c93b7232510
WBRK-dev <koertjrweberp@gmail.com>
2025-05-30 15:57:22 +0200

    fix: automatically create db file if it does not exist

--- src/Services/CheckIfDaemonCanRun.ts
+++ src/Services/CheckIfDaemonCanRun.ts
@@ -1,5 +1,9 @@
+import { spawn } from "node:child_process";
 import NotLoggedInError from "../Kernel/Classes/Errors/NotLoggedInError.js";
+import Db from "../Kernel/Support/Facades/Db.js";
 import Print from "../Kernel/Support/Facades/Print.js";
+import fs from "node:fs";
+import path from "node:path";
 
 export default async function () {
     Print.warn("You are not logged in.");
@@ -7,4 +11,10 @@ export default async function () {
     Print.info("Waiting for 120 seconds before exiting...");
     // await new Promise(resolve => setTimeout(resolve, 120000));
     // throw new NotLoggedInError();
+
+    if (!fs.existsSync(path.resolve(Db().client.config.connection.filename))) {
+        Print.info("Database file not found. Creating a new database...");
+        const child = spawn("node", ["build/", "db", "migrate"], { stdio: "inherit" });
+        await new Promise(r => child.on("exit", r));
+    }
 }
