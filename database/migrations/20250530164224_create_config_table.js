e53ce865a95cfdf3d09e9fae8015d28d1b377862
WBRK-dev <koertjrweberp@gmail.com>
2025-05-30 18:47:58 +0200

    feat: added config migration

---
+++ database/migrations/20250530164224_create_config_table.js
@@ -0,0 +1,21 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function up(knex) {
+    return knex.schema.createTable('config', (table) => {
+        table.increments('id').primary();
+        table.string('key').notNullable().unique();
+        table.text('value').notNullable();
+        table.timestamp('created_at').defaultTo(knex.fn.now());
+        table.timestamp('updated_at').defaultTo(knex.fn.now());
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function down(knex) {
+    return knex.schema.dropTableIfExists('config');
+};
